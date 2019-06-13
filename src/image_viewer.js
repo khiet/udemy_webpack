dc2d6755be721440b24409e9ee376fe944f58514
Khiet Le <khiet.le@ehochef.com>
2019-06-13 09:55:19 +0100

    another js file to add an image to body

---
+++ src/image_viewer.js
@@ -0,0 +1,4 @@
+const image = document.createElement('img');
+image.src = 'https://picsum.photos/id/1037/400/400';
+
+document.body.append(image);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import sum from './sum';
+import './image_viewer';
 
 const output = sum(1, 2);
 console.log(output);
